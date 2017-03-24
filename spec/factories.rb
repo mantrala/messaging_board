ce99de20c35e81174a5303435037f7355e40fe3b
Sarath Mantrala <sarath23@gmail.com>
2017-03-24 10:16:06 -0700

    Added devise registration and login
    * This resulted in tests failing because they are unauthenticated.
    * Added devise capability to tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    email "first@abc.com"
+    password "rubyme"
+    first_name "Richard"
+    last_name "Schneems"
+  end
+end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
+    it "goes to the index when user is logged in" do
+      user = create(:user)
+      sign_in user
+
       get posts_path
       expect(response).to have_http_status(200)
+    end
+
+    it "redirects to login page if user is not authneticated" do
+      get posts_path
+
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_url)
     end
   end
 end
